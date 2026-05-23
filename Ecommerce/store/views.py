84d442b8755d17228d85fe16652d9658b20d0873
Rupesh Rana <rupeshrana20274@gmail.com>
2026-05-24 00:44:04 +0530

    add pagination logic and also logic in html page (store.html) and store.views.py file

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -13,23 +17,32 @@ def store(request, category_slug=None):
         product_count = products.count()
     
     else:
-
         products = Product.objects.all().filter(is_available=True)
-        product_count = products.count()
+
+    # paginator 
+    paginator = Paginator(products, 2) # 6 products per page
+    page = request.GET.get('page') # get the page number from the url (?page=2)
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
 
     context = {
-        'products' : products,
-        'product_count' : product_count
+        'products' : paged_products,
+        'product_count' : product_count,
     }
     return render(request, 'store/store.html', context)
 
 def product_details(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        # this code is for when an item is already in cart then show already in cart
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        # return HttpResponse(in_cart)
     except Exception as e:
         raise e
     
     context = {
-        'single_product' : single_product
+        'single_product' : single_product,
+        'in_cart' : in_cart,
+        
     }
     return render(request, 'store/product_details.html', context)
